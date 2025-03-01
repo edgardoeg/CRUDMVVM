e50b0e48b5d22bd9d3c2c0332ffd390515d541ab
edgardoeg <edgardoeg@yahoo.com>
2025-02-28 18:12:49 -0600

    Add project files.

---
+++ CRUDMVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CRUDMVVM/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CRUDMVVM
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
